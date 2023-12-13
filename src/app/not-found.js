af9fd49cc3228d0a0f8485d01dc251d7839b44d0
Zolzaya Udval <23LP6080@G301-CC0173.local>
2023-12-13 11:55:30 +0800

    ppp

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,3 +1,31 @@
+import { Container } from "@/components/Container";
+import Link from "next/link";
+
 export default function NotFound() {
-    return <>404</>
+  return (
+    <>
+      <Container>
+        <div className="w-full flex justify-center items-center">
+          <div className="w-[70%]">
+            <div className="flex gap-[40px]">
+              <div className="text-[120px]">404</div>
+              <hr className="border-[2px]" />
+              <div className="flex flex-col justify-between">
+                <div>Page not Found</div>
+                <div>
+                  We're sorry, This page is unknown or does not exist the page
+                  you are looking for.
+                </div>
+                <Link href={"/#"}>
+                  <div className="bg-sky-400 rounded text-[white] w-fit p-[10px]">
+                    Back to Home
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
